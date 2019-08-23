09607e89ca8052170a5731bd161d8efa34a79223
JowitaKK <jowita.knap@wp.pl>
2019-08-24 00:20:04 +0100

    default value

--- Hello_props/Hello.js
+++ Hello_props/Hello.js
@@ -10,5 +10,23 @@ class Hello extends React.Component {
         );
     }
 
-
 }
+
+/* default value
+class Hello extends React.Component{
+    static defaultProps={
+        from: 'Anonymous',
+        bangs: 
+    }
+        render(){
+        let bangs = "!".repeat(this.props.bangs); //this.props.bangs or by method
+        return (
+            <div>
+              <p>Hi {this.props.to} from {this.props.from}{bangs}</p>
+              <img src={this.props.img}/>
+
+            </div>
+        );
+    }
+}
+*/
