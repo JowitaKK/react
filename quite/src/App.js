acaf1c9d2a3f2fb019548b7524ee8e56b426a21a
JowitaKK <jowita.knap@wp.pl>
2019-09-20 17:33:03 +0100

    using axios, check URL

---
+++ quite/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ZenQuote from './ZenQuote';
+import './App.css';
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <ZenQuote/>
+    </div>
+    );
+  } 
+}
+export default App;
