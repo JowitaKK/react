3211abc242b135adf0675e8e64655641c08ac75d
JowitaKK <jowita.knap@wp.pl>
2019-11-09 21:39:44 +0000

    fake client side routing,do not have to change url

---
+++ react-router/src/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className = 'Contact'>
+                <h1>Contact!!!</h1>
+                <p>Please do contact us.</p>
+            </div>
+        )
+    }
+}
+export default Contact;
