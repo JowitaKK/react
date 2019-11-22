6eb270232c65824ededfd3848ecc6e97afe41e6f
JowitaKK <jowita.knap@wp.pl>
2019-11-22 23:42:34 +0000

    frame of redux app

---
+++ todo_redux/src/components/Todo.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { connect } from "react-redux";
+import cs from "classnames";
+import { toggleTodo } from "../redux/actions";
+
+const Todo = ({ todo, toggleTodo }) => (
+    <li className="todo-item" onClick={() => toggle Todo(todo.id)} >
+        
+)
